9a9ff53ab75a32fd83c93c766be3aeece792dc73
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-03-25 13:03:02 -0600

    Agrega nuevo componente ClimaItem

--- src/components/ClimaExtendido.js
+++ src/components/ClimaExtendido.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types'
 import climaInfoExtra from './LocacionClima/DatosClima/ClimaInfoExtra';
+import ClimaItem from './ClimaItem/index'
 
 class ClimaExtendido extends Component {
     render() {
         const {ciudad} = this.props;
 
         return (
-            <div>Pronostico extendido para {ciudad}</div>
+            <div>
+                Pronostico extendido para {ciudad}
+                <ClimaItem></ClimaItem>
+            </div>
         )
     }
 }
