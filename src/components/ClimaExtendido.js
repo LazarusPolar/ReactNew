a32e4f9ac2e7998c424550cf54f2045a71a55b14
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-03-25 11:49:09 -0600

    Agrega funcionalidad basica de modulo de clima extendido

--- src/components/ClimaExtendido.js
+++ src/components/ClimaExtendido.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types'
+import climaInfoExtra from './LocacionClima/DatosClima/ClimaInfoExtra';
 
 class ClimaExtendido extends Component {
     render() {
+        const {ciudad} = this.props;
+
         return (
-            <div>Pronostico extendido</div>
+            <div>Pronostico extendido para {ciudad}</div>
         )
     }
+}
+
+ClimaExtendido.propTypes = {
+    ciudad: PropTypes.string.isRequired
 }
 
 export default ClimaExtendido;
