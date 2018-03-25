8d5f6575962a5ec4b48c6331048fd846249320ff
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-03-24 22:53:49 -0600

    Sube nivel de envio de datos en jerarquia

--- src/components/LocacionClima/DatosClima/index.js
+++ src/components/LocacionClima/DatosClima/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ClimaTemperatura from './ClimaTemperatura'
 import ClimaInfoExtra from './ClimaInfoExtra'
+import PropTypes from 'prop-types'
 import {
     CLOUD,
     CLOUDY,
@@ -11,13 +12,23 @@ import {
  } from './../../../constants/climas'
  import './styles.css'
 
-const datosClima = () => {
+const datosClima = ({ datos }) => {
+    const {temperatura, estadoClima, humedad, viento} = datos;
     return (
         <div className = 'datosClimaCont'> 
-            <ClimaTemperatura temperatura = {10} estadoClima = {CLOUD}> </ClimaTemperatura>
-            <ClimaInfoExtra humedad = {80} viento = {'10 m/s'}> </ClimaInfoExtra>
+            <ClimaTemperatura temperatura = {temperatura} estadoClima = {CLOUD}> </ClimaTemperatura>
+            <ClimaInfoExtra humedad = {humedad} viento = {viento}> </ClimaInfoExtra>
         </div>
     )
+}
+
+datosClima.propTypes = {
+    data: PropTypes.shape({
+        temperatura: PropTypes.number.isRequired,
+        estadoClima: PropTypes.string.isRequired,
+        humedad: PropTypes.number.isRequired,
+        viento: PropTypes.string.isRequired
+    })
 }
 
 export default datosClima;
--- src/components/LocacionClima/index.js
+++ src/components/LocacionClima/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import DatosClima from './DatosClima/index'
 import Locacion from './Locacion'
+import {
+    CLOUD,
+    CLOUDY,
+    SUN, 
+    RAIN, 
+    SNOW, 
+    WINDY
+ } from './../../constants/climas'
+
+const datos = {
+    temperatura: 20, 
+    estadoClima: SUN,
+    humedad: 10, 
+    viento: '10 m/s'
+}
 
 const locacionClima = () => {
     return (
         <div>
             <Locacion ciudad = {'Ciudad de Mexico'}> </Locacion>
-            <DatosClima> </DatosClima>
+            <DatosClima datos = {datos} /> 
         </div>   
     )
 }
