2900dc87d2cae5f8f2c456ebe4f16a1d2aae1d85
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-03-25 17:19:45 -0600

    Crea carpeta y archivo de Actions

--- src/App.js
+++ src/App.js
@@ -5,15 +5,12 @@ import Paper from 'material-ui/Paper'
 import ListaLocacion from './components/ListaLocacion'
 import ClimaExtendido from './components/ClimaExtendido'
 import {createStore} from 'redux'
+import {setCiudad} from './actions/index'
 import './App.css';
 
 const ciudades = ['Buenos Aires,AR', 'Mexico,MX', 'Madrid,ES'];
 const store = createStore(() => {}, window. __REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-const setCiudad = value => ({
-  type: 'setCiudad',
-  value
-})
 class App extends Component {
 
   constructor() {
---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+export const SET_CIUDAD = 'SET_CIUDAD'
+
+export const setCiudad = value => ({
+    type: SET_CIUDAD,
+    value
+  })
