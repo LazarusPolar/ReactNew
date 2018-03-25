be9d0d4acbd15b5a183d04e51dc249483524d8ba
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-03-25 14:03:03 -0600

    Agrega implementacion de Moment para cambiar las fechas del formato Unix

--- src/services/tranformaForecasts.js
+++ src/services/tranformaForecasts.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import moment from 'moment'
+import 'moment/locale/es'
+import transformacionClima from './../services/transformacionClima'
 
 const TransformaForecast = datos => (
     datos.list.filter(
@@ -8,7 +10,12 @@ const TransformaForecast = datos => (
             moment.unix(item.dt).hour() === 12 ||
             moment.unix(item.dt).hour() === 18
         )
-    )
+    ).map(item => (
+        {
+        diaSemana: moment.unix(item.dt).format('ddd'),
+        hora: moment.unix(item.dt).hour(),
+        datos: transformacionClima(item)
+    }))
 )
 
 export default TransformaForecast
